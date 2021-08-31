81c3496fa6a388944bd2a627d241cb99acff1139
Michael Ballantyne <michael.ballantyne@gmail.com>
2021-08-31 17:31:34 -0400

    add support for pvars w/ nested lists in bspec expand

--- binding-spec/expand.rkt
+++ binding-spec/expand.rkt
@@ -5,8 +5,22 @@
 
 (require
   racket/match
+  racket/list
   ee-lib
   "spec.rkt")
+
+;; maps over a tree
+;;
+;; tree := (listof tree)
+;;       | any/c
+(define-syntax-rule
+  (for/tree ([item init]) body ...)
+  (let for-nested ([list-at-depth init])
+    (let ([item init])
+      (if (list? item)
+          (for/list ([nested item])
+            (for-nested nested))
+          (begin body ...)))))
 
 ;; `exp-state` is (hashof symbol? syntax?)
 
@@ -18,18 +32,21 @@
 (define (simple-expand-internal spec exp-state local-scopes)
   (match spec
     [(ref (? svar? pv) (? procedure? pred) (? string? msg))
-     (define id (hash-ref exp-state pv))
-     (when (not (lookup id pred))
-       (raise-syntax-error #f msg id))
+     (define ids (hash-ref exp-state pv))
+     (for ([id (flatten ids)])
+       (when (not (lookup id pred))
+         (raise-syntax-error #f msg id)))
      exp-state]
     [(subexp (? svar? pv) (? nonterm? f))
      (hash-update exp-state pv
-                  (lambda (stx)
-                    (f (add-scopes stx local-scopes))))]
+                  (lambda (stxs)
+                    (for/tree ([stx stxs])
+                      (f (add-scopes stx local-scopes)))))]
     [(bind (? svar? pv) (? procedure? valc))
      (hash-update exp-state pv
-                  (lambda (stx)
-                    (bind! (add-scopes stx local-scopes) (valc))))]
+                  (lambda (stxs)
+                    (for/tree ([stx stxs])
+                      (bind! (add-scopes stx local-scopes) (valc)))))]
     [(export (list-rest (? svar? pvs)) (? qualifier?))
      (error 'simple-expand "export specs not supported")]
     [(scope spec)
