e2369d244a543c4f7cc1a462640743cc2d47e033
Michael Ballantyne <michael.ballantyne@gmail.com>
2021-09-17 16:47:00 -0400

    add (broken) define* test

---
+++ test/define-star.rkt
@@ -0,0 +1,52 @@
+#lang racket/base
+
+(require "../main.rkt"
+         rackunit
+         (for-syntax racket/base syntax/parse racket/pretty))
+
+(define-hosted-syntaxes
+  (binding-class var "mylang variable")
+
+  (extension-class mylang-macro)
+
+  (nonterminal expr
+    #:description "mylang expression"
+    #:allow-extension mylang-macro
+
+    v:var
+    n:number
+    (+ e1:expr e2:expr)
+    
+    (block d:def-or-expr ...)
+    #:binding (nest d []))
+
+  (nonterminal def-or-expr (tail)
+    #:description "mylang definition context"
+    #:allow-extension mylang-macro
+
+    (begin d:def-or-expr ...)
+    #:binding (nest d tail)
+    
+    (define* v:var e:expr)
+    #:binding [e {(! v) tail}]
+    
+    e:expr))
+
+;; simulated interface macro
+(define-syntax mylang-expr
+  (syntax-parser
+    [(_ e) #`'#,((nonterminal-expander expr) #'e)]))
+
+;; sugar
+
+;; tests
+(check-equal?
+ (mylang-expr
+  (block
+   (define* x 5)
+   (define* x (+ x 1))
+   (+ x 1)))
+ '(block
+   (define* x 5)
+   (define* x (+ x 1))
+   (+ x 1)))
