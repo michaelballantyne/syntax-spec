4aa3e85a3af7109815bc4cc5daf9fc8c5ade4899
Michael Ballantyne <michael.ballantyne@gmail.com>
2022-03-02 09:18:51 -0500

    export number from syntax parse

--- main.rkt
+++ main.rkt
@@ -1,5 +1,7 @@
 #lang racket/base
 
-(provide (all-from-out "private/syntax/interface.rkt"))
+(provide (all-from-out "private/syntax/interface.rkt")
+         (for-syntax number))
 
-(require "private/syntax/interface.rkt")
+(require "private/syntax/interface.rkt"
+         (for-syntax syntax/parse))
