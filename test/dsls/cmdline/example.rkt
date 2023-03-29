876192bbcbb228dad976a2ffcd7800e9ea0b159f
Michael Ballantyne <michael.ballantyne@gmail.com>
2023-03-29 12:17:35 -0400

    don't run example in test

--- test/dsls/cmdline/example.rkt
+++ test/dsls/cmdline/example.rkt
@@ -7,17 +7,18 @@
       str
       (raise-user-error "expected path to exisiting file")))
 
-(define/command-line-options
-  #:options
-  [verbose-mode (switch/o ("-v" "--verbose") "Compile with verbose messages")]
-  [profiling-on (switch/o ("-p" "--profile") "Compile with profiling")]
-  [optimize-level
-   (choice/o #:default 0
-             ["--optimize-level" [lvl (int-range/p 0 3)]
-                                 "set optimization level to <lvl>" lvl]
-             (numbered-flags/f "--o" [0 3] "optimization level"))]
-  [output
-   (required/o "-o" outfile "the output filename" outfile)]
-  [link-flags (list/o ["-l" "--link-flags"] lf "Add a flag <lf> for the linker")]
-  #:arguments
-  [file-to-compile existing-file/p])
+(module+ main
+  (define/command-line-options
+    #:options
+    [verbose-mode (switch/o ("-v" "--verbose") "Compile with verbose messages")]
+    [profiling-on (switch/o ("-p" "--profile") "Compile with profiling")]
+    [optimize-level
+      (choice/o #:default 0
+                ["--optimize-level" [lvl (int-range/p 0 3)]
+                 "set optimization level to <lvl>" lvl]
+                (numbered-flags/f "--o" [0 3] "optimization level"))]
+    [output
+      (required/o "-o" outfile "the output filename" outfile)]
+    [link-flags (list/o ["-l" "--link-flags"] lf "Add a flag <lf> for the linker")]
+    #:arguments
+    [file-to-compile existing-file/p]))
