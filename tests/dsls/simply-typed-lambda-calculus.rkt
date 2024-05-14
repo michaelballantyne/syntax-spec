f51da30264d192be444d72af8ec592507fd82e13
Mike Delmonaco <mthundershock@gmail.com>
2024-05-14 13:54:28 -0400

    simply typed lambda calculus dsl

---
+++ tests/dsls/simply-typed-lambda-calculus.rkt
@@ -0,0 +1,196 @@
+#lang racket/base
+
+(require "../../testing.rkt"
+         (for-syntax racket/match))
+
+(syntax-spec
+  (binding-class typed-var)
+  (extension-class typed-macro #:binding-space stlc)
+  (nonterminal typed-expr
+    #:allow-extension typed-macro
+    #:binding-space stlc
+
+    x:typed-var
+    n:number
+
+    (lambda ([x:typed-var (~datum :) t:type] ...) body:typed-expr)
+    #:binding (scope (bind x) body)
+    (#%app fun:typed-expr arg:typed-expr ...)
+
+    (let ([x:typed-var e:typed-expr] ...) body:typed-expr)
+    #:binding (scope (bind x) body)
+
+    ; type annotation
+    (~> (e (~datum :) t)
+        #'(: e t))
+    ((~datum :) e:typed-expr t:type)
+
+    ; rewrite for tagging applications
+    (~> (fun arg ...)
+        #'(#%app fun arg ...)))
+  (nonterminal type
+    Number
+    ((~literal ->) arg-type:type ... return-type:type))
+  #;#;
+  (nonterminal/exporting typed-definition
+    #:allow-extension typed-macro
+    #:binding-space stlc
+    (#%define x:typed-var t:type e:typed-expr)
+    #:binding (export x)
+    (begin defn:typed-definition-or-expr ... e:typed-expr)
+    #:binding (re-export defn))
+  (nonterminal/exporting typed-definition-or-expr
+    #:allow-extension typed-macro
+    #:binding-space stlc
+    defn:typed-definition
+    #:binding (re-export defn)
+    e:typed-expr)
+  (host-interface/expression
+   (stlc/expr e:typed-expr)
+   (infer-expr-type #'e)
+   #'(compile-expr e))
+  (host-interface/expression
+   (stlc/infer e:typed-expr)
+   (define t (infer-expr-type #'e))
+   (define t-datum (type->datum t))
+   #`'#,t-datum))
+
+(begin-for-syntax
+  ; a Type is one of
+  (struct number-type [] #:prefab)
+  (struct function-type [arg-types return-type] #:prefab)
+  ; arg-types is a (listof Type)
+
+  ; maps identifiers to types
+  (define-persistent-symbol-table types)
+
+  ; Syntax -> Type
+  (define (infer-expr-type e)
+    (syntax-parse e
+      [n:number (number-type)]
+      [x:id (symbol-table-ref types #'x (lambda () (raise-syntax-error 'infer-expr-type "untyped identifier" #'x)))]
+      [((~datum lambda) ([x:id _ t] ...) body)
+       (define arg-types (map parse-type (attribute t)))
+       (for ([x (attribute x)]
+             [t arg-types])
+         (symbol-table-set! types x t))
+       (define body-type (infer-expr-type #'body))
+       (function-type arg-types body-type)]
+      [((~datum #%app) f arg ...)
+       (define f-type (infer-expr-type #'f))
+       (match f-type
+         [(function-type expected-arg-types return-type)
+          (unless (= (length expected-arg-types) (length (attribute arg)))
+            (raise-syntax-error 'infer-expr-type
+                                (format "arity error. expected ~a arguments, but got ~a"
+                                        (length expected-arg-types)
+                                        (length (attribute arg)))
+                                this-syntax))
+          (for ([expected-type expected-arg-types]
+                [arg (attribute arg)])
+            (check-expr-type arg expected-type))
+          return-type]
+         [_ (raise-syntax-error 'infer-expr-type
+                                (format "type mismatch. expected a function type, but got ~a"
+                                        (type->datum f-type))
+                                #'f)])]
+      [((~datum :) e t-stx)
+       (define t (parse-type #'t-stx))
+       (check-expr-type #'e t)
+       t]
+      [((~datum let) ([x e] ...) body)
+       (for ([x (attribute x)]
+             [e (attribute e)])
+         (symbol-table-set! types x (infer-expr-type e)))
+       (infer-expr-type #'body)]))
+
+  ; Syntax Type -> Void
+  (define (check-expr-type e expected-type)
+    (define actual-type (infer-expr-type e))
+    (unless (equal? expected-type actual-type)
+      (raise-syntax-error 'infer-expr-type
+                          (format "type mismatch. expected ~a, but got ~a"
+                                  (type->datum expected-type)
+                                  (type->datum actual-type))
+                          e)))
+
+  ; Syntax -> Type
+  (define (parse-type t-stx)
+    (syntax-parse t-stx
+      [(~datum Number) (number-type)]
+      [((~datum ->) arg-type ... return-type)
+       (function-type (map parse-type (attribute arg-type))
+                      (parse-type #'return-type))]))
+
+  ; Type -> any
+  ; converts to simple s-expression for displaying
+  (define (type->datum t)
+    (match t
+      [(number-type) 'Number]
+      [(function-type arg-types return-type)
+       (append (list '->)
+               (map type->datum arg-types)
+               (list (type->datum return-type)))])))
+
+(define-syntax compile-expr
+  (syntax-parser
+    [(_ n:number) #'n]
+    [(_ x:id) #'x]
+    [(_ ((~datum lambda) ([x:id _ _] ...) body))
+     #'(lambda (x ...) (compile-expr body))]
+    [(_ ((~datum #%app) f arg ...))
+     #'((compile-expr f) (compile-expr arg) ...)]
+    [(_ ((~datum :) e _)) #'(compile-expr e)]
+    [(_ ((~datum let) ([x e] ...) body))
+     #'(let ([x (compile-expr e)] ...) (compile-expr body))]))
+
+(define-syntax define-stlc-syntax
+  (syntax-parser
+    [(_ name:id trans:expr)
+     #`(define-syntax #,((make-interned-syntax-introducer 'stlc) #'name 'add)
+         (typed-macro trans))]))
+
+(define-stlc-syntax let*
+  (syntax-parser
+    [(_ () body) #'(let () body)]
+    [(_ ([x:id e] binding ...) body)
+     #'(let ([x e]) (let* (binding ...) body))]))
+
+; testing
+
+(define-syntax-rule
+  (check-eval e v)
+  (check-equal? (stlc/expr e) v))
+(define-syntax-rule
+  (check-infer e t)
+  (check-equal? (stlc/infer e) 't))
+(check-eval 1 1)
+(check-eval ((lambda ([x : Number]) x) 1) 1)
+(check-infer (lambda ([x : Number]) x)
+             (-> Number Number))
+(check-eval (let ([x 1]) x) 1)
+(check-eval
+ (let* ([second (lambda ([x : Number] [y : Number]) y)]
+        [x (second 1 2)])
+   x)
+ 2)
+(check-exn
+ #rx"expected a function type, but got Number"
+ (lambda ()
+   (convert-compile-time-error
+    (stlc/expr (1 2)))))
+(check-exn
+ #rx"arity error. expected 0 arguments, but got 1"
+ (lambda ()
+   (convert-compile-time-error
+    (stlc/expr ((lambda () 1) 2)))))
+(check-exn
+ #rx"type mismatch. expected Number, but got \\(-> Number\\)"
+ (lambda ()
+   (convert-compile-time-error
+    (stlc/expr ((lambda ([x : Number]) x) (lambda () 1))))))
+(check-exn
+ #rx"type mismatch. expected Number, but got \\(-> Number\\)"
+ (lambda ()
+   (convert-compile-time-error
+    (stlc/expr ((lambda () 1) : Number)))))
