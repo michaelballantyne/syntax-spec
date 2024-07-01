5381049ed2eb3e18a708a889a9c1046717021ff6
Michael Ballantyne <michael.ballantyne@gmail.com>
2024-07-01 12:53:26 -0400

    lift disappeareds on literals / form names

--- private/runtime/syntax-classes.rkt
+++ private/runtime/syntax-classes.rkt
@@ -3,13 +3,14 @@
 (provide literal-in-space
          ~space-literal)
 
-(require syntax/parse "../ee-lib/main.rkt"
+(require syntax/parse "../ee-lib/main.rkt" (for-template "../ee-lib/lift-disappeareds.rkt")
          (for-syntax racket/base syntax/parse))
 
 (define-syntax-class (literal-in-space target-id binding-space)
   (pattern id:id
            #:when (same-binding? ((in-space binding-space) (attribute id))
-                                 ((in-space binding-space) target-id))))
+                                 ((in-space binding-space) target-id))
+           #:do [(lift-disappeared-uses! ((in-space binding-space) (attribute id)))]))
 
 (define-syntax ~space-literal
   (pattern-expander
